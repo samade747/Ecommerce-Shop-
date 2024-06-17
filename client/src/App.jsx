30f5c782b1f74ef2113457a40c7e9abd8b8735f0
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-06-18 02:47:53 +0500

    247am 18 06 2024

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,53 @@
 import React from 'react';
-import Home from './pages/Home';
-import ProductList from './pages/ProductList';
-import Product from './pages/Product';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Cart from './pages/Cart';
+import Home from './pages/Home.jsx';
+import ProductList from './pages/ProductList.jsx';
+import Product from './pages/Product.jsx';
+import Register from './pages/Register.jsx';
+import Login from './pages/Login.jsx';
+import Cart from './pages/Cart.jsx';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 const App = () => {
-  return <Home />
-    // <div>
-    //   
-    // </div>
+  return (
+    <Router>
+      <Switch>
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+
+        <Route path="/cart">
+          <Cart />
+        </Route>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/register">
+          <Register />
+        </Route>
+
+      </Switch>
+
+    </Router>
+
+  )
   
 };
 
