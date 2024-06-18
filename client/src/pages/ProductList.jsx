bf9adbbc60b855e668d79dc849e2729a2512181e
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-06-18 07:02:58 +0500

    702

--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -1,115 +1,97 @@
-import React from 'react'
-import styled from 'styled-components'
-import Navbar from '../components/Navbar'
-import Announcement from '../components/Announcement'
-import Products from '../components/Products'
-import Newsletter from '../components/Newsletter'
-import Footer from '../components/Footer'
-import { mobile } from '../responsive'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
+import Announcement from '../components/Announcement';
+import Products from '../components/Products';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
+import { mobile } from '../responsive';
+import { useLocation } from 'react-router-dom';
 
-
-
-const Container = styled.div`  
-  
-
-${mobile({ flexDirection: 'column' })}
-`
+const Container = styled.div`
+  ${mobile({ flexDirection: 'column' })}
+`;
 
 const Title = styled.h1`
   margin: 20px;
-  
   ${mobile({ fontSize: '24px' })}
-`
+`;
 
 const FilterContainer = styled.div`
-
   display: flex;
   justify-content: space-between;
-
   ${mobile({ flexDirection: 'column' })}
-`
+`;
 
 const Filter = styled.div`
   margin: 20px;
-
   ${mobile({ width: '0px 20px', display: 'flex', flexDirection: 'column' })}
+`;
 
-
-`
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
-
-
   ${mobile({ marginRight: '0px' })}
-`
+`;
+
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
-  
-
   ${mobile({ margin: '10px 0px' })}
+`;
 
-
-`
 const Option = styled.option`
-    ${mobile({ margin: '10px 0px' })}
-
-`
-
-
+  ${mobile({ margin: '10px 0px' })}
+`;
 
 const ProductList = () => {
   const location = useLocation();
   const category = location.pathname.split("/")[2];
+  const [filters, setFilters] = useState({});
+  const [sort, setSort] = useState("newest");
 
-  
   return (
-      <Container>
-        <Navbar />
-        <Announcement />
-        <Title>Dresses</Title>
-        <FilterContainer>
-          <Filter>
-            <FilterText>Filter Products:</FilterText>
-            <Select>
-              <Option disabled selected>
-                Color
-              </Option>
-              <Option>White</Option>
-              <Option>Black</Option>
-              <Option>Red</Option>
-              <Option>Blue</Option>
-              <Option>Yellow</Option>
-              <Option>Green</Option>
-            </Select>
-            <Select>
-              <Option disabled selected>
-                Size
-              </Option>
-              <Option>XS</Option>
-              <Option>S</Option>  
-              <Option>M</Option>
-              <Option>L</Option>
-              <Option>XL</Option>
-              <Option>XXL</Option>  
-            </Select>
-          </Filter>
-          <Filter>
-            <FilterText>Sort Products:</FilterText>
-            <Select>
-              <Option selected>Newest</Option>
-              <Option>Price (asc)</Option>
-              <Option>Price (desc)</Option>
-            </Select>
-          </Filter> 
-        </FilterContainer>
-        <Products />
-        <Newsletter />
-        <Footer />
-      </Container>
-  )
-}
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Title>{category}</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+          <Select name="color" onChange={(e) => setFilters({ ...filters, [e.target.name]: e.target.value })}>
+            <Option disabled>Color</Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
+          </Select>
+          <Select name="size" onChange={(e) => setSort(e.target.value)}>
+            <Option disabled>Size</Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+            <Option>XXL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select name="sort" onChange={(e) => setSort(e.target.value)}>
+            <Option value="newest" selected>Newest</Option>
+            <Option value="asc">Price (asc)</Option>
+            <Option value="desc">Price (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products category={category} filters={filters} sort={sort} />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
 
-export default ProductList
+export default ProductList;
