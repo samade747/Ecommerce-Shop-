c0e7e2afaf8ccd2d172af1e39a1b85f84df23f39
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-06-30 00:33:47 +0500

    Update requestMethods.js

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const baseURL = "http://localhost:5000/api/";
+const Token = 
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYzQxYzIyYjQ1YzI0MzkwNzU1NjU5ZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NjQ3NzI3N30.0n9h7k6kzS7JqSf6Eo7H9OuZ6pWkXyZpV'
 
 
 export const publicRequest = axios.create({ 
